981b4c3bba0bc9090a740ff3f20017f9c6f7baa6
DongheeL <donghlee19@gmail.com>
2020-12-24 18:29:19 +0900

    쿼리스트링 값 추출

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query; //url의 쿼리스트링(url에서 ?이하 부분)을 추출할 수 있다 .
+    console.log('id = '+queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       response.writeHead(404);
       response.end();
       return;
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+
+    response.end(queryData.id); //출력할 페이지에 들어갈 내용
 
 });
 app.listen(3000);
