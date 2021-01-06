4a8ec0454e3936bd474b37a30af518ed078b42a3
DongheeL <donghlee19@gmail.com>
2021-01-06 16:57:18 +0900

    data 입력 페이지 생성

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
   <body>
   <h1><a href="/">WEB</a></h1>
   ${list}
+  <a href="/create">create</a>
   ${body}
   </body>
   </html>
@@ -32,6 +33,7 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query; //url의 쿼리스트링(url에서 ?이하 부분)을 추출할 수 있다 .
     var pathname = url.parse(_url, true).pathname;
+    console.log(pathname);
     var title = queryData.id;
 
     if(pathname ==='/'){
@@ -56,10 +58,28 @@ var app = http.createServer(function(request,response){
           });
       });
     }
+  }else if (pathname === '/create') {
+    fs.readdir('./data',function(error, filelist){
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list,`
+        <form class="" action="http://localhost:3000/process_create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `);
+      response.writeHead(200);
+      response.end(template); //출력할 페이지에 들어갈 내용
+    })
   }else{
       response.writeHead(404);
       response.end('Not found'); //출력할 페이지에 들어갈 내용
-    }
+  }
 
 });
 app.listen(3000);
